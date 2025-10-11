6c8d0b06dc29ee42d9adc16d08344bb90a695389
rushin-gh <rushin.mailme@gmail.com>
2025-10-11 17:07:53 +0530

    Basic setup complete

---
+++ frontend/src/app.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom/client";
+
+const testElement = <div id="testElement">Test element</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(testElement);
