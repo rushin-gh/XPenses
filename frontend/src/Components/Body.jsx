6772422759c2b777f1976952a93719fd29e012bd
rushin-gh <rushin.mailme@gmail.com>
2025-10-14 00:58:06 +0530

    Create Input fields for adding new expense

--- frontend/src/Components/Body.jsx
+++ frontend/src/Components/Body.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
 import ExpensesList from "../Data/ExpenseList.json";
 
 const Body = () => {
-  console.log(ExpensesList);
+  const [expense, setExpense] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect(() => {
+    Fun();
+  }, []);
+
+  async function Fun() {
+    const ex = await fetch("http://localhost:3000/expense", {
+      method: "GET",
+    });
+    const exx = await ex.json();
+    console.log(exx);
+  }
+
   return (
     <div id="body">
+      <div id="expenseInput">
+        <input
+          id="expenseTxt"
+          type="text"
+          placeholder="Enter expense"
+          value={expense}
+          onChange={(event) => setExpense(event.target.value)}
+        />
+        <input
+          id="expenseAmt"
+          type="text"
+          placeholder="Enter amount"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
+
+        <button type="submit">Add</button>
+      </div>
       <table>
         <thead>
           <tr>
